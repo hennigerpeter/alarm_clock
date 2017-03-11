eba5396b9b5815f650a9601b74e28fb25f00326b
Peter Henniger <henniger.peter@gmail.com>
2017-03-11 11:25:23 +0100

    int2ascii korrigiert

--- hdl/int2ascii.vhd
+++ hdl/int2ascii.vhd
@@ -6,7 +6,8 @@ USE IEEE.numeric_std.all;
 ENTITY int2ascii IS
     Port ( i_number : IN  integer RANGE 0 TO 59;
            o_ascii0   : OUT std_logic_vector(7 downto 0);
-           o_ascii1   : OUT std_logic_vector(7 downto 0);
+           o_ascii1   : OUT std_logic_vector(7 downto 0)
+           );
 END ENTITY int2ascii;
 
 ARCHITECTURE behavioral OF int2ascii IS
@@ -22,16 +23,16 @@ BEGIN
         v_bcd1 := "0000";
         v_number := to_unsigned(i_number, v_number'length);
 
-        -- Nur einen Durchlauf machen, da v_bcd0 und v_bcd1 nicht per Loop durchlaufen werden kÃ¶nnen
+        -- Nur einen Durchlauf machen, da v_bcd0 und v_bcd1 nicht per Loop durchlaufen werden können
         FOR i IN 0 TO 1 LOOP
             --TODO: Pseudocode umsetzen
 
-            -- Zahl groeÃŸer 4, addiere 3
+            -- Zahl größer 4, addiere 3
             if(v_bcd0>"100") then
                 v_bcd0 := v_bcd0 + 3;
             end if;
             
-            -- Zahl groeÃŸer 4, addiere 3
+            -- Zahl größer 4, addiere 3
             if(v_bcd0>"100") then 
                 v_bcd0 := v_bcd0 + 3;
             end if;
@@ -49,7 +50,7 @@ BEGIN
         s_bcd1 <= v_bcd1;
     END PROCESS;
     o_ascii0(3 DOWNTO 0) <= std_logic_vector(s_bcd0);
-    o_ascii0(7 DOWNTO 0) <= "0011" -- ASCII Ziffern 0-9 entsprechen den Zahlen 30-39, es wird demnach eine 3 in der linken BCD vorangestellt
+    o_ascii0(7 DOWNTO 0) <= "0011"; -- ASCII Ziffern 0-9 entsprechen den Zahlen 30-39, es wird demnach eine 3 in der linken BCD vorangestellt
     o_ascii1 <= std_logic_vector(s_bcd1);
-    o_ascii1(7 DOWNTO 0) <= "0011" -- ASCII Ziffern 0-9 entsprechen den Zahlen 30-39, es wird demnach eine 3 in der linken BCD vorangestellt
+    o_ascii1(7 DOWNTO 0) <= "0011"; -- ASCII Ziffern 0-9 entsprechen den Zahlen 30-39, es wird demnach eine 3 in der linken BCD vorangestellt
 END ARCHITECTURE behavioral;
